183ae2fd0eee5d30b74ec557d949c78425d83488
Joshua Yates <josht.yates@btinternet.com>
2018-08-07 14:21:55 +0100

    Installed C# / .NET packages and created a C# project that will be updated with testing of new concepts.

---
+++ CSharpLearningApp/CSharpLearningApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, world!");
+            System.Console.ReadLine();  //Prevent closing of window
+        }
+    }
+}
