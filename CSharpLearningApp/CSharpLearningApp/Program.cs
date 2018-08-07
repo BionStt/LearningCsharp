255788e10f50f3bd1c8feb41c5c06c2ac1a57ea8
Joshua Yates <josht.yates@btinternet.com>
2018-08-07 16:58:38 +0100

    Added a test of the DateTime structure with printing of date, day name, month name and current time.

--- CSharpLearningApp/CSharpLearningApp/Program.cs
+++ CSharpLearningApp/CSharpLearningApp/Program.cs
@@ -11,6 +11,11 @@ namespace CSharpLearningApp
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello, world!");
+            System.DateTime CurrentTime = System.DateTime.Now;
+            System.String MonthName = System.DateTime.Now.ToString("MMMM");
+            System.String OutputString = "Today is " + CurrentTime.DayOfWeek + ", " + CurrentTime.Day.ToString() + " of " + MonthName + ", " + CurrentTime.Year.ToString()
+                + "\n" + "The current time is " + CurrentTime.Hour.ToString() + ":" + CurrentTime.Minute.ToString();
+            System.Console.WriteLine(OutputString);
             System.Console.ReadLine();  //Prevent closing of window
         }
     }
