c3effddf878ce02af0feef7eac2c655d0a86b76e
Joshua Yates <josht.yates@btinternet.com>
2018-08-08 14:15:07 +0100

    Finished bohr model program by adding a string validation function and energy calculation function. Also added formatting for Joules output before the results are printed.

--- BohrModel/BohrModel/Program.cs
+++ BohrModel/BohrModel/Program.cs
@@ -4,7 +4,7 @@ namespace BohrModel
 {
     class Program
     {
-        private static int TakeIntegerInput(string VariableName)
+        private static int TakePositiveIntegerInput(string VariableName)
         {
             bool GettingInput = true;
             int InputValue = 0;
@@ -13,14 +13,64 @@ namespace BohrModel
                 System.Console.Write("Please enter an integer value for " + VariableName + ": ");
                 if (int.TryParse(System.Console.ReadLine(), out InputValue))
                 {
-                    break;
+                    if (InputValue > 0)
+                    {
+                        break;
+                    }
                 }
                 System.Console.WriteLine("Invalid input, please try again");
             }
             return InputValue;
         }
+
+        private static string TakeStringInput(string[] Options, string CustomMessage)
+        {
+            string InputString = "InputString";
+            string LoweredString = "LoweredString";
+            bool GettingInput = true;
+            while (GettingInput)
+            {
+                System.Console.Write(CustomMessage);
+                InputString = System.Console.ReadLine();
+                LoweredString = InputString.ToLower();
+                bool ValidInputFound = false;
+                foreach(string Option in Options)
+                {
+                    if(LoweredString == Option.ToLower())
+                    {
+                        //Valid option found
+                        ValidInputFound = true;
+                        break;
+                    }
+                }
+                if (ValidInputFound)
+                {
+                    break;
+                }
+                //Else no matching input found, so give an error message and try again
+                System.Console.WriteLine("Invalid input, please input one of the following:");
+                foreach(string Option in Options)
+                {
+                    System.Console.WriteLine(Option);
+                }
+            }
+            return InputString;
+        }
+
+        private static double CalculateEnergy(int n1, int n2, double z, string unit)
+        {
+            double EnergyDifference = 13.6 * (System.Math.Pow(z, 2)) * ((1 / (System.Math.Pow(n2, 2)) - (1 / (System.Math.Pow(n1, 2)))));
+            if (unit.ToLower() == "j")
+            {
+                EnergyDifference *= 1.6e-19;
+            }
+            return EnergyDifference;
+        }
+
         static void Main()
         {
+            string[] UnitList = new string[] { "eV", "J" };
+            string[] ContinueOptions = new string[] { "Y", "N" };
             //Intro message displayed on startup
             string IntroMessage = "Welcome to the bohr model energy calculator\n" +
                 "You can calculate the energy of a photon produced by the transition of\n" +
@@ -33,24 +83,47 @@ namespace BohrModel
             {
                 System.Console.WriteLine(IntroMessage);
                 //Take input of variables
-                int z = TakeIntegerInput("z");
-                int n1;
-                int n2;
+                int z = TakePositiveIntegerInput("z");
+                int n1 = 1;
+                int n2 = 1;
                 //n1 must be greater than n2
                 bool ComparingN = true;
                 while (ComparingN)
                 {
-                    n1 = TakeIntegerInput("n1");
-                    n2 = TakeIntegerInput("n2");
+                    n1 = TakePositiveIntegerInput("n1");
+                    n2 = TakePositiveIntegerInput("n2");
                     if (n1 > n2)
                     {
                         break;
                     }
                     System.Console.WriteLine("n1 must be larger than n2");
                 }
-                CloseProgram = true;    //TEST
+                //At this point, z, n1 and n2 are valid. Take input of units
+                string Unit = TakeStringInput(UnitList, "Please choose the units (eV or J): ");
+                double EnergyDifference = CalculateEnergy(n1, n2, z, Unit);
+                //Format units for displaying
+                string PrintedUnits = "error";
+                switch (Unit.ToLower())
+                {
+                    case "ev":
+                        PrintedUnits = "eV";
+                        break;
+                    case "j":
+                        //With 3 decimal places precision, most answers would come out as 0J, so make it a bit more useful
+                        EnergyDifference *= 1e19;
+                        PrintedUnits = "x e-19 J";
+                        break;
+                }
+                string ResultsMessage = "For a proton number of " + z.ToString() + " the photon energy from transition between levels " + n1.ToString() + " and " + n2.ToString() + " is:\n" +
+                    System.Math.Round(EnergyDifference, 3).ToString() + " " + PrintedUnits;
+                System.Console.WriteLine(ResultsMessage);
+                string ProgramContinueChoice = TakeStringInput(ContinueOptions, "Would you like to restart the program? (Y/N): ");
+                if (ProgramContinueChoice.ToLower() == "n")
+                {
+                    CloseProgram = true;
+                }
+                System.Console.Clear();
             }
-            System.Console.ReadLine();  //TEST
         }
     }
 }
