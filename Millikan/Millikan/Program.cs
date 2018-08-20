c2ea8cbb118b7f587694eb708e8ff8faeb000396
Joshua Yates <josht.yates@btinternet.com>
2018-08-20 19:47:28 +0100

    Started translating a previous C++ program for reading experimental data to C#. Added a list of required functions.

---
+++ Millikan/Millikan/Program.cs
@@ -0,0 +1,42 @@
+/*Recreation of previous C++ program to read data for millikan's experiment from a text file and calculate mean, standard deviation
+ and standard error for charge on an electron. Any erroneous data is discarded*/
+
+namespace Millikan
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+
+        //Function to check a string is a double
+
+        //Function to validate input with list of input options
+
+    }
+
+    class FileHandler
+    {
+        /*STATIC FUNCTIONS*/
+
+        //Function to check file exists
+
+        //Function to open user file
+
+        //Function to calculate file length
+
+        //Function to read each line in a file to an array
+    }
+
+    class StatsCalculator
+    {
+        /*STATIC FUNCTIONS*/
+
+        //Function to calculate mean
+
+        //Function to calculate standard deviation
+
+        //Function to calculate standard error
+    }
+}
