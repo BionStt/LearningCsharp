1e3ba1858fce0679b8856a78d4f32614f886ecb9
Joshua Yates <josht.yates@btinternet.com>
2018-09-17 16:58:28 +0100

    Added a test in main of reading and displaying all contents of a text file.

--- Millikan/Millikan/Program.cs
+++ Millikan/Millikan/Program.cs
@@ -1,13 +1,26 @@
 /*Recreation of previous C++ program to read data for millikan's experiment from a text file and calculate mean, standard deviation
  and standard error for charge on an electron. Any erroneous data is discarded*/
 
+
 namespace Millikan
 {
     class Program
     {
         static void Main()
         {
-
+            System.Console.Write("Please enter a file path: ");
+            string Result = "File does not exist";
+            string FilePath = System.Console.ReadLine();
+            if (FileHandler.FileExists(FilePath))
+            {
+                Result = "File exists";
+            }
+            System.Console.WriteLine(Result);
+            if (FileHandler.FileExists(FilePath))
+            {
+                System.Console.WriteLine(System.IO.File.ReadAllText(FilePath));
+            }
+            System.Console.Read();
         }
 
         //Function to check a string is a double
@@ -21,6 +34,14 @@ namespace Millikan
         /*STATIC FUNCTIONS*/
 
         //Function to check file exists
+        public static bool FileExists(string FileName)
+        {
+            if (System.IO.File.Exists(FileName))
+            {
+                return true;
+            }
+            return false;
+        }
 
         //Function to open user file
 
